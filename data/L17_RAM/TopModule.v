eb15fd242a9e19f1d73b2567fdb895cb9db17162
kazu-321 <kzs321kzs@gmail.com>
2026-01-22 09:05:11 +0900

    add report

--- data/L17_RAM/TopModule.v
+++ data/L17_RAM/TopModule.v
@@ -23,12 +23,11 @@ module TopModule(
 	m_chattering u0(CLK1,BTN[0],w_btn0);
 	assign w_we=~w_btn0;
 	
-	m_ram u1(SW[9:6],SW[3:0],w_we,rdata);
+	m_ram u1(SW[9:4],SW[3:0],w_we,rdata);
 	assign LED=10'h0;
 	m_seven_segment s0(rdata,HEX0);
 	m_seven_segment s1(SW[3:0],HEX1);
-	m_seven_segment s2(SW[9:6],HEX2);
-	assign HEX3=8'hff;
+	m_seven_segment_2 s2(SW[9:4],HEX2, HEX3);
 	assign HEX4=8'hff;
 	assign HEX5=8'hff;
 	
